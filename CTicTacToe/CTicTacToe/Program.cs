2aa070019f3d580aacd5fcca54dce2cf81e2c038
NillaWafers <bltconnect@gmail.com>
2015-09-30 08:34:12 -0400

    BoardCreation

--- CTicTacToe/CTicTacToe/Program.cs
+++ CTicTacToe/CTicTacToe/Program.cs
@@ -8,9 +8,24 @@ namespace CTicTacToe
 {
 	class Program
 	{
+        //Array for available places on the board.
+        static char[] board = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
 		static void Main(string[ ] args)
 		{
             //test commit
 		}
+        // Board method which creats board  
+        private static void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[1], board[2], board[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[4], board[5], board[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[7], board[8], board[9]);
+            Console.WriteLine("     |     |      ");
+        }
 	}
 }
