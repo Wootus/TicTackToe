7b137f619f09f67b6787a274650ab4d66ce065b8
Aaron Selonke <aarondv1@gmail.com>
2016-06-28 11:27:43 -0400

    Board array-index fixed

--- CTicTacToe/CTicTacToe/Program.cs
+++ CTicTacToe/CTicTacToe/Program.cs
@@ -13,18 +13,22 @@ namespace CTicTacToe
 		static void Main(string[ ] args)
 		{
             //test commit
+
+            Board();
+            Console.ReadKey();
+
 		}
         // Board method which creats board  
         private static void Board()
         {
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", board[1], board[2], board[3]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[0], board[1], board[2]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", board[4], board[5], board[6]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[3], board[4], board[5]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", board[7], board[8], board[9]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[6], board[7], board[8]);
             Console.WriteLine("     |     |      ");
         }
 	}
